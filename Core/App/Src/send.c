ec37f4160cbfad68555087b243359a764cd07a9f
Max-unterwegs <14068827+max_unterwegs@user.noreply.gitee.com>
2025-02-15 19:52:14 +0800

    通信协议适配上位机

--- Core/App/Src/send.c
+++ Core/App/Src/send.c
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-02-04 10:54:03
  * @LastEditors: Max-unterwegs && max_unterwegs@126.com 
- * @LastEditTime: 2025-02-12 11:54:00
+ * @LastEditTime: 2025-02-15 19:43:19
  * @FilePath: \MDK-ARMd:\Mein Werk\meine code\stm32projekt\scope\Core\App\Src\send.c
  */
 #include "send.h"
@@ -18,7 +18,10 @@ void sendvolt(float *sendbuffer)
     // tx_pack.floats[0] = sendbuffer[0];
     // tx_pack.floats[1] = sendbuffer[1];
     // sendValuePack(&tx_pack);
-    // printf("CH1_voltage: %f, CH2_voltage: %f, verfint_voltage: %f\r\n", sendbuffer[0], sendbuffer[1], sendbuffer[2]);
+
+
+        printf("CH1:%fV,CH2:%fV.\r\n", sendbuffer[0], sendbuffer[1]);
+
 }
 
 void sendres(float *sendbuffer)
